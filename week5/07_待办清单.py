6351582441a3e8ac022b93ec8e46657f4d36d3dc
Wenwu Huang <hww508944725@gmail.com>
2026-06-26 21:30:20 +0800

    搭好菜单骨架，4个分支齐，能退出

---
+++ week5/07_待办清单.py
@@ -0,0 +1,16 @@
+todos = [{"content":"买菜", "done":True}, {"content":"做饭", "done":False}, {"content":"洗碗", "done":False}]
+
+while True:
+    print("1.查看全部", "2.添加", "3.删除", "4.退出")
+    choice = input("用户输入的数字")
+    if choice == "1":
+        print("查看全部")
+    elif choice == "2":
+        print("添加")
+    elif choice == "3":
+        print("删除")
+    elif choice == "4":
+        print("退出")
+        break
+    else:
+        print("无效输入")
