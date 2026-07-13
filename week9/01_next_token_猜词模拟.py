97803e192d7964c102338e854b4eb1cfc6bb4c88
Wenwu Huang <hww508944725@gmail.com>
2026-07-13 20:34:29 +0800

    注释01_next_token 猜词概率模拟

--- week9/01_next_token_猜词模拟.py
+++ week9/01_next_token_猜词模拟.py
@@ -1,8 +1,8 @@
-import random
+import random                   # 导入 random 模块，用来做随机选择
 a = {"好": 0.3, "差": 0.25, "热": 0.25, "蓝": 0.2}
 
-words = list(a.keys())
-probs = list(a.values())
+words = list(a.keys())          # 把字典里的键取出来，变成列表
+probs = list(a.values())        # 把字典里的值取出来，变成列表
 
-for i in range(20):
+for i in range(20):     # 重复 20 次，每次按照上面的概率随机选出一个字并打印
     print(random.choices(words, weights=probs)[0])
